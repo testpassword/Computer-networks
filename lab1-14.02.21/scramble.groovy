cbe9f153a79b847cbef5f35d0ca86eaf6a3cf459
Kulbako <kulbakowork@mail.ru>
2021-03-16 19:46:11 +0300

    completed lab1

--- lab1-14.02.21/scramble.groovy
+++ lab1-14.02.21/scramble.groovy
@@ -1,6 +1,3 @@
-@Grab('commons-collections:commons-collections:3.2.1')
-import static org.apache.commons.collections.CollectionUtils.*
-
 List<String> code = this.args[0].toCharArray().collect { it.toString().toBoolean() }
 def polynomial = { x, y, z = null -> if (z) x ^ y ^ z else x ^ y }
 def scramble = { arr, a, b ->
@@ -9,7 +6,7 @@ def scramble = { arr, a, b ->
         else if (i >= b) polynomial(el, code[i - a], code[i - b])
         else el
     }}
-def toReadable = { [(it.collect { n -> n ? 1 : 0 }.join()): disjunction(code, it).size()] }
+def toReadable = { (it.collect { n -> n ? 1 : 0 }.join()) }
 println([[3, 5], [5, 7]]
         .collect { scramble(code, it[0], it[1]) }
-        .collectEntries { toReadable it  })
+        .collect{ toReadable it  })
